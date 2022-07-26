d4805438db387bb8eefc099081816d2001d0014f
yasslne <mdyassinechiboub@gmail.com>
2022-07-26 21:46:08 +0100

    modified the form and added states

--- clinique/models/models.py
+++ clinique/models/models.py
@@ -2,27 +2,34 @@
 
 from odoo import models, fields, api
 
-
 class Docteur(models.Model):
     _name = 'clinique.docteur'
     _description = 'Modele du docteur'
 
-    nom = fields.Char()
-    prenom = fields.Char()
-    age = fields.Integer()
+    name = fields.Char()
+    date_docteur = fields.Date('Date de naissance', default=fields.Datetime.now)
     specialite = fields.Selection(
         [('cardiologie', 'Cardiologie'),
          ('reanimation', 'Réanimation'),
          ('urgence', 'Urgence')],
         'Spécialité')
     patient_id = fields.One2many('clinique.patient', 'docteur_id')
+    state = fields.Selection([
+        ('name', 'Name'),
+        ('dateDeNaissance', 'Date de naissance'),
+        ('specialite', 'Spécialité'),
+    ], string='Status', readonly=True, default='name')
+    # print(fields.Datetime.to_string(date_docteur))
+    # print("---------------date---------------")
+    # print(type(date_docteur))
+    # print(date_docteur)
+    # print("---------------date---------------")
 
 
 class Patient(models.Model):
     _name = 'clinique.patient'
     _description = 'Modele du pati1ent'
 
-    nom = fields.Char()
-    prenom = fields.Char()
-    age = fields.Integer()
+    name = fields.Char()
+    date_patient = fields.Date('Date de naissance', default=fields.Datetime.now)
     docteur_id = fields.Many2one('clinique.docteur')
