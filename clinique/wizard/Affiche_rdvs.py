2e713e1c7037d8668e7b8360bc6494504ec65b82
YASSlNE <mdyassinechiboub@gmail.com>
2022-08-03 10:08:27 +0100

    debugging the RDVs wizard

--- clinique/wizard/Affiche_rdvs.py
+++ clinique/wizard/Affiche_rdvs.py
@@ -6,9 +6,12 @@ from odoo import api, fields, models
 class AfficheRdvs(models.TransientModel):
     _name = 'clinique.rdvs.affiche'
     _description = 'Affichage des rendez vous sous forme pdf'
-    docteur_id = fields.Many2one('clinique.docteur', 'Docteur')
-
+    docteur_id = fields.Many2one('clinique.docteur')
+    _inherit = 'clinique.rdv'
     def action_print_rdvs(self):
-        data = self.env['clinique.rdvs.affiche'].browse(self.env.context.get('active_ids'))
-        print(data)
+        # print(docteur_id)
+        data = self.env['clinique.rdv'].search([('docteur', '=', 'self.docteur_id')])
+        for d in data:
+            print(d['date'])
+            print('mlsqdkjfqsdmlkfjqs')
         return None
