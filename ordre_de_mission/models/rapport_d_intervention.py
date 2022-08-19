dc5d0409b8d3f3d1bb4519b1ab745a881bf20773
YASSlNE <mdyassinechiboub@gmail.com>
2022-08-19 11:55:00 +0100

    Control of input

--- ordre_de_mission/models/rapport_d_intervention.py
+++ ordre_de_mission/models/rapport_d_intervention.py
@@ -9,51 +9,107 @@ class RapportIntervention(models.Model):
     mission_id = fields.Many2many('ordre.mission', domain="[('employee_id', 'in', employes), ('projet_id', '=', projet_id), ('client_id', '=', client_id)]")
     client_id = fields.Many2one('res.partner')
     projet_id = fields.Many2one('project.project')
-    date_begin = fields.Datetime()
-    date_end = fields.Datetime()
+    date_begin = fields.Datetime(required=True)
+    date_end = fields.Datetime(required=True)
     employes = fields.Many2many('hr.employee')
     clients = fields.Many2many('res.partner', domain="[('parent_id', '=', client_id)]" )
     line_t_ids = fields.One2many('rapport.intervention.lines.taches', 'rit_id')
 
+    duree = fields.Char()
 
-    # @api.onchange('projet_id')
-    # def fn(self):
-    #     print("-------------------------------------")
-    #     print(self.projet_id._ids)
-    #     print("-------------------------------------")
+    
 
 
-    # @api.depends('employes2')
-    # def _compute_employes(self):
-    #     for line in self:
-    #         line.employes=line.employes2._origin.id
-
-
+    # @api.onchange('mission_id')
+    # def missions(self):
+    #     def intersection(list1, list2):  #Retourne l'intersection entre deux listes
+    #         return list(set(list1) & set(list2))
+    #     # for k in self.mission_id.ids:
+    #     #     print(k)
+    #     records = self.env['ordre.mission'].search([('id', 'in', self.mission_id.ids)])
+    #     # for i in inspect.getmembers(records):
+    #     #     if not i[0].startswith('_'):
+    #     #         if not inspect.ismethod(i[1]): 
+    #     #             print(i)
+    #     employee_ids = []
+    #     for r in records:
+    #         employee_ids.append(r['employee_id'].id)
+    #     print(f"employee ids are {employee_ids}")
+    #     print(self.employes.ids)
+    #     if(len(self.employes.ids)!=len(intersection(self.employes.ids, employee_ids))):
+    #         print("ERRORRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR")        
 
     @api.model
     def create(self, values):
-        print(f"the mission id is {values['mission_id']}")
-        records = self.env['ordre.mission'].search([('id', '=', values['mission_id'])])
-        records.ensure_one()
-        dateLimitMin = datetime.strftime(records['heureDebut'], "%Y-%m-%d")
-        dateLimitMax = datetime.strftime(records['heureFin'], "%Y-%m-%d")
+        print(f"the mission id is {values['mission_id'][0][2]}")
+        records = self.env['ordre.mission'].search([('id', 'in', values['mission_id'][0][2])])
 
 
-        # conversion des dates
         dateInputMinConverted = datetime.strptime(values['date_begin'], "%Y-%m-%d %H:%M:%S")
         dateInputMaxConverted = datetime.strptime(values['date_end'], "%Y-%m-%d %H:%M:%S")
 
 
-        dateInputMin = datetime.strftime(dateInputMinConverted, "%Y-%m-%d")
-        dateInputMax = datetime.strftime(dateInputMaxConverted, "%Y-%m-%d")
-        if(dateInputMin<dateLimitMin or rdateInputMax>dateLimitMax):
-            raise exceptions.ValidationError("Intervale des dates invalide")
+        def CheckDatesValidation(record):
+            dateLimitMin = datetime.strftime(record['heureDebut'], "%Y-%m-%d")
+            dateLimitMax = datetime.strftime(record['heureFin'], "%Y-%m-%d")
+
+            # conversion des dates
+
+            dateInputMin = datetime.strftime(dateInputMinConverted, "%Y-%m-%d")
+            dateInputMax = datetime.strftime(dateInputMaxConverted, "%Y-%m-%d")
+            return (dateInputMin<dateLimitMin or dateInputMax>dateLimitMax)
+
+        def not_intersection(list1, list2):
+            return list(set(list1) ^ set(list2))
+        def intersection(list1, list2):  #Retourne l'intersection entre deux listes
+            return list(set(list1) & set(list2))
+        # for k in self.mission_id.ids:
+        #     print(k)
+        # records2 = self.env['ordre.mission'].search([('id', 'in', self.mission_id.ids)])
+        # # for i in inspect.getmembers(records):
+        # #     if not i[0].startswith('_'):
+        # #         if not inspect.ismethod(i[1]): 
+        # #             print(i)
+        employee_ids = [] #liste des employés qui sont dans les missions sélectionnés
+        for r in records:  
+            employee_ids.append(r['employee_id'].id)
+        # print(f"employee ids are {employee_ids}")
+        # print(self.employes.ids)
+        # return super(RapportIntervention, self).create(values)
+        employee_ids_selected = values['employes'][0][2]
+        Error = False
+        for r in records:
+            Error=CheckDatesValidation(r)
+            if(Error):
+                break
+
+        # if(dateInputMin<dateLimitMin or dateInputMax>dateLimitMax):
+        #     raise exceptions.ValidationError("Intervale des dates invalide")
+        # else:
+        #     duree = dateInputMaxConverted-dateInputMinConverted+timedelta(days=1)
+        #     values['duree'] = duree
+        #     res = super(RapportIntervention, self).create(values)
+        #     return res
+        # print(employee_ids_selected)
+
+        # print(employee_ids)
+        # print(intersection(employee_ids_selected, employee_ids))
+        print(f"Error value {Error}")
+        if(len(employee_ids_selected)!=len(intersection(employee_ids_selected, employee_ids))):
+            ErrorCausedBy = not_intersection(employee_ids_selected, employee_ids)
+            ErrorCausedByNames = []
+            getNames = self.env['hr.employee'].search([('id', 'in', ErrorCausedBy)])
+            for r in getNames:
+                print("qskldjfhlqsdk")
+                ErrorCausedByNames.append(r.name)
+            raise exceptions.ValidationError(f"Les missions de {ErrorCausedByNames} ne sont pas inclus")
+        elif(Error):
+            raise exceptions.ValidationError(f"L'intervalle des dates d'un rapport d'intervention doit être dans l'intersection des dates des ordres de mission sélectionnés")
         else:
             duree = dateInputMaxConverted-dateInputMinConverted+timedelta(days=1)
             values['duree'] = duree
             res = super(RapportIntervention, self).create(values)
             return res
-
 
 class RapportInterventionLinesTaches(models.Model):
     _name = 'rapport.intervention.lines.taches'
@@ -61,7 +117,15 @@ class RapportInterventionLinesTaches(models.Model):
     description = fields.Char()
     status = fields.Selection([('E', 'En cours'), ('C', 'Clôturé')])
     employes = fields.Many2one('hr.employee')
+    @api.onchange('employes')
+    def fn(self):
+        if(self.employes.id not in self.rit_id.employes.ids):
+            raise exceptions.ValidationError(f"L'employé doit être un des employés choisi")
 
+    @api.model
+    def create(self, values):
+        print(values['employes'])
+        print(type(values['employes']))      
 
 class StoreEmployees(models.Model):
     _name = 'rapport.store.employees'
